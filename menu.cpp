34832a8428519b6d631377a93b24d7c164552a56
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-06-19 20:01:41 +0530

    First programme in C++

---
+++ menu.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    //cout << "Hello World!!" ;
+    cout << "Enter a number :" << endl ;
+    int a;
+    cin >> a;
+    cout << "You Entered :" << a << endl ;
+};
