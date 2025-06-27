304eba3ff7d9bae60d2f1e46ceee1c527893c237
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-06-27 22:22:30 +0530

    Printing some more patterns

--- menu.cpp
+++ menu.cpp
@@ -8,6 +8,38 @@ int main(){
     // cin >> a;
     // cout << "You Entered :" << a << endl ;
 
+    //Printing a rectangle
+    cout << "Enter length:"<< endl;
+    int a;
+    cin>>a;
+    cout << "Enter breadth:"<< endl;
+    int b;
+    cin>>b;
+    // for(int row=0; row<a; row++){
+    //     for(int col=0; col<b; col++){
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+    // similar for square length and breadth are same
+
+    //Printing hollow rectangle or square
+    for(int row=0; row<a; row++){
+        if(row==0 || row==a-1){
+        for(int col=0; col<b; col++){
+            cout << "* ";
+        }
+        }else{
+            cout<< "* ";
+            for(int col=0; col<b-2; col++){
+                cout << "  ";
+            }
+            cout<< "* ";
+        }
+        cout << endl;
+    }    
+
+
     // Printing hollow hollow half-pyramid
     // cout << "Enter number of rows:" << endl;
     // int n;
@@ -47,26 +79,26 @@ int main(){
     // }
 
     // Printing hollow pyramid
-    cout << "Enter number of rows:" << endl;
-    int n;
-    cin>>n;
-    for(int row=0; row<n; row++){
-        int k=0;
-       for(int col=0; col< 2*n+1; col++){
-        if(col<n-row-1){
-            cout << " ";
-        } else if(k<2*row+1){
-            if(k==0 || k==2*row || row==n-1){
-              cout<< "*";
-            }else{
-                cout<< " ";
-        }
-            k++;
-         }
-          else{
-            cout << " ";
-        }
-       }
-        cout<<endl;
-    }
+    // cout << "Enter number of rows:" << endl;
+    // int n;
+    // cin>>n;
+    // for(int row=0; row<n; row++){
+    //     int k=0;
+    //    for(int col=0; col< 2*n+1; col++){
+    //     if(col<n-row-1){
+    //         cout << " ";
+    //     } else if(k<2*row+1){
+    //         if(k==0 || k==2*row || row==n-1){
+    //           cout<< "*";
+    //         }else{
+    //             cout<< " ";
+    //     }
+    //         k++;
+    //      }
+    //       else{
+    //         cout << " ";
+    //     }
+    //    }
+    //     cout<<endl;
+    // }
 };
