07d0bdaba13dcb85d421fe691075f7bb70aca9c8
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-07-02 21:53:47 +0530

    .

--- menu.cpp
+++ menu.cpp
@@ -200,36 +200,36 @@ int main(){
     //    }
 
     // Flipped solid daimond
-    int n;
-    cin >> n;
-    for(int row=0; row<n; row++){
-      // for half pyramid
-      for(int col=0; col<n-row; col++){
-        cout << "*";
-      }
-      // For spaces
-      for(int col=0; col<2*row+1; col++){
-        cout << " ";
-      }
-      // second half pyramid
-      for(int col=0; col<n-row; col++){
-        cout << "*";
-      }
-      cout << endl;
-    }
-    for(int row=0; row<n; row++){
-      // for half pyramid
-      for(int col=0; col<row+1; col++){
-          cout << "*";
-      }
-      // for spaces
-      for(int col=0; col<2*n-2*row-1; col++){
-        cout << " ";
-      }
-      // For second half pyramid
-      for(int col=0; col<row+1; col++){
-          cout << "*";
-      }
-      cout<<endl;
-    }
+    // int n;
+    // cin >> n;
+    // for(int row=0; row<n; row++){
+    //   // for half pyramid
+    //   for(int col=0; col<n-row; col++){
+    //     cout << "*";
+    //   }
+    //   // For spaces
+    //   for(int col=0; col<2*row+1; col++){
+    //     cout << " ";
+    //   }
+    //   // second half pyramid
+    //   for(int col=0; col<n-row; col++){
+    //     cout << "*";
+    //   }
+    //   cout << endl;
+    // }
+    // for(int row=0; row<n; row++){
+    //   // for half pyramid
+    //   for(int col=0; col<row+1; col++){
+    //       cout << "*";
+    //   }
+    //   // for spaces
+    //   for(int col=0; col<2*n-2*row-1; col++){
+    //     cout << " ";
+    //   }
+    //   // For second half pyramid
+    //   for(int col=0; col<row+1; col++){
+    //       cout << "*";
+    //   }
+    //   cout<<endl;
+    // }
 };
