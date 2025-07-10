6b6229039d1544490c104ab4ef54e3a05a97424b
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-07-10 21:51:27 +0530

    Learning pre & post increment and decrement , and switch case statement

--- menu.cpp
+++ menu.cpp
@@ -339,6 +339,22 @@ int main(){
 //                 }
         
 //    }
-//     cout << endl;
+//   cout << endl;
 // }
+
+// Switch case
+cout  << "Enter the number" << endl;
+int val;
+cin >> val;
+switch(val){
+     case 1: cout << "Love";
+     break;
+     case 2: cout << "Rahul";
+     break;
+     case 3: cout << "Virat";
+     break;
+     case 4: cout << "Rohit";
+     break;
+     default: "suresh";
+}
 };
