c8b78034be49cb131c89aa6aee205773d697a995
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-08-05 23:50:06 +0530

    Findind pairs and triplet for sum in an Array

--- menu.cpp
+++ menu.cpp
@@ -769,33 +769,60 @@ int main(){
 // }
 
 // Intersection with duplicates
-int arr[]={1,2,4,6,8,10};
-int sizea = 6;
-int brr[]={3,4,5,6};
-int sizeb = 4;
+// int arr[]={1,2,4,6,8,10};
+// int sizea = 6;
+// int brr[]={3,4,5,6};
+// int sizeb = 4;
 
-vector<int>ans;
-for(int i=0; i<sizea; i++){
-    int element = arr[i];
-    for(int j=0; j<sizeb; j++){
-        if(element == brr[j]){
-            brr[j] = INT_MIN;
-        }
+// vector<int>ans;
+// for(int i=0; i<sizea; i++){
+//     int element = arr[i];
+//     for(int j=0; j<sizeb; j++){
+//         if(element == brr[j]){
+//             brr[j] = INT_MIN;
+//         }
+//     }
+// }
+
+// for(int i=0; i<sizea; i++){
+//     ans.push_back(arr[i]);
+// }
+// for(int i=0; i<sizeb; i++){
+//     if(brr[i] != INT_MIN){
+//     ans.push_back(brr[i]);
+//     }
+// }
+
+// // Printing ans array
+// for(int i=0; i<ans.size(); i++){
+//     cout << ans[i] << " ";
+// }
+
+// Printing pair with sum as ans
+// vector<int>arr{1,2,3,4,5,6,7};
+// int sum=9;
+// for(int i=0; i<arr.size(); i++){
+//     int element = arr[i];
+//     for(int j=i+1; j<arr.size(); j++){
+//         if(arr[i] + arr[j] == sum){
+//             cout << "(" << element << "," << arr[j] << ")" << endl ;
+//         }
+//     }
+// }
+
+// Printing triplet with sum as ans
+vector<int>arr{10,20,30,40};
+int sum = 80;
+for(int i=0; i<arr.size(); i++){
+    int element1 = arr[i];
+    for(int j=i+1; j<arr.size(); j++){
+    int element2 = arr[j];
+    for(int k=j+1; k<arr.size(); k++){
+    int element3 = arr[k];
+    if(element1 + element2 + element3 == sum){
+        cout << "(" << element1 << "," << element2 << "," << element3 << ")" << endl;
     }
+  }
+ }
 }
-
-for(int i=0; i<sizea; i++){
-    ans.push_back(arr[i]);
-}
-for(int i=0; i<sizeb; i++){
-    if(brr[i] != INT_MIN){
-    ans.push_back(brr[i]);
-    }
-}
-
-// Printing ans array
-for(int i=0; i<ans.size(); i++){
-    cout << ans[i] << " ";
-}
-
 };
