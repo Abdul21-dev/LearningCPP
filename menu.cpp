da49d1f6b5f0f13f612811388be49f36191f8d60
Abdul21-dev <abdulrahman21062004@gmail.com>
2025-08-22 23:31:13 +0530

    .

--- menu.cpp
+++ menu.cpp
@@ -1061,13 +1061,13 @@ int main(){
 // }
 
 // Initialising 2D Array
-// int row = 3;
-// int col = 5;
-// vector<vector<int> >arr(row, vector<int>(col, 18));
-// for(int i=0; i<arr.size(); i++){
-//     for(int j=0; j<arr[i].size(); j++){
-//         cout << arr[i][j] << " ";
-//     }
-//     cout << endl;
-// }
+int row = 3;
+int col = 5;
+vector<vector<int> >arr(row, vector<int>(col, 18));
+for(int i=0; i<arr.size(); i++){
+    for(int j=0; j<arr[i].size(); j++){
+        cout << arr[i][j] << " ";
+    }
+    cout << endl;
+}
 };
