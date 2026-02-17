095d10f2e7fcc49433f5dfbe61e166a20c33379c
Abdul21-dev <abdulrahman21062004@gmail.com>
2026-02-17 10:39:02 +0530

    Leetcode 3798

--- menu.cpp
+++ menu.cpp
@@ -2660,15 +2660,27 @@ int main(){
     // }
 
     // Leetcode 1346
-     bool checkIfExist(vector<int>& arr) {
-        for(int i=0; i<arr.size(); i++){
-            for(int j=0; j<arr.size(); j++){
-                if(arr[i] == 2*arr[j] && i != j){
-                    return true;
-                }
+    //  bool checkIfExist(vector<int>& arr) {
+    //     for(int i=0; i<arr.size(); i++){
+    //         for(int j=0; j<arr.size(); j++){
+    //             if(arr[i] == 2*arr[j] && i != j){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // Leetcode 3798
+     string largestEven(string s) {
+        for(int i=s.length()-1; i>=0; i--){
+            if(s[i] == '1'){
+                s.pop_back();
+            }else{
+                break;
             }
         }
-        return false;
+        return s;
     }
  }
 
