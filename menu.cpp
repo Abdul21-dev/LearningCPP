1205487ab97b167d7e9e4c9b23b511f206a427a5
Abdul21-dev <abdulrahman21062004@gmail.com>
2026-02-21 16:01:42 +0530

    Leetcode 1832

--- menu.cpp
+++ menu.cpp
@@ -2698,20 +2698,36 @@ int main(){
     // }
 
     // Leetcode 2540
-     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i=0;
-        int j=0;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]==nums2[j]){
-                return nums1[i];
-            }else if(nums1[i]<nums2[j]){
-                i++;
-            }else{
-                j++;
+    //  int getCommon(vector<int>& nums1, vector<int>& nums2) {
+    //     int i=0;
+    //     int j=0;
+    //     while(i<nums1.size() && j<nums2.size()){
+    //         if(nums1[i]==nums2[j]){
+    //             return nums1[i];
+    //         }else if(nums1[i]<nums2[j]){
+    //             i++;
+    //         }else{
+    //             j++;
+    //         }
+    //     }
+        
+    //     return -1;
+    // }
+
+    // Leetcode 1832
+     bool checkIfPangram(string sentence) {
+        int hash[26]={0};
+        for(int i=0; i<sentence.size(); i++){
+            if(sentence[i]>='a' && sentence[i]<='z'){
+            hash[sentence[i]-'a']++;
             }
         }
-        
-        return -1;
+        for(int i=0; i<26; i++){
+            if(hash[i]==0){
+                return false;
+            }
+        }
+        return true;
     }
  }
 
