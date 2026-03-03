1088db499c38ea6525af590a8b2ef583cc1089c5
Abdul21-dev <abdulrahman21062004@gmail.com>
2026-03-03 15:00:31 +0530

    Leetcode 169 & 229

--- menu.cpp
+++ menu.cpp
@@ -2973,5 +2973,76 @@ int main(){
         }
         return candidate;
     }
+
+    // Leetcode 229
+     vector<int> majorityElement(vector<int>& nums) {
+    //     unordered_map<int,int>hash;
+    //     set<int>arr;
+    //     for(int i=0; i<nums.size(); i++){
+    //         hash[nums[i]]++;
+    //         if(hash[nums[i]]>(nums.size()/3)){
+    //             arr.insert(nums[i]);
+    //         }
+    //     }
+    //     vector<int>ans;
+    //     for(int x: arr){
+    //         ans.push_back(x);
+    //     }
+    //     return ans;
+    // }
+
+    //  unordered_map<int,int>hash;
+    //     vector<int>arr;
+    //     for(int i=0; i<nums.size(); i++){
+    //         hash[nums[i]]++;
+    //     }
+    //     for(auto p: hash){
+    //         if(p.second > nums.size()/3){
+    //             arr.push_back(p.first);
+    //         }
+    //     }
+       
+    //     return arr;
+
+    int count1 = 0;
+    int count2 = 0;
+    int candidate1 = 0;
+    int candidate2 = 0;
+    for(int i=0; i<nums.size(); i++){
+        if(candidate1 == nums[i]){
+            count1++;
+        }else if(candidate2 == nums[i]){
+            count2++;
+        }else if(count1 == 0){
+            candidate1 = nums[i];
+            count1=1;
+        }else if(count2 == 0){
+            candidate2 = nums[i];
+             count2=1;
+        }else{
+            count1--;
+            count2--;
+        }
+    }
+    
+    count1 =0;
+    count2 = 0;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i]==candidate1){
+            count1++;
+        }else if(nums[i]==candidate2){
+            count2++;
+        }
+    }
+
+    vector<int>ans;
+    if(count1>nums.size()/3){
+        ans.push_back(candidate1);
+    }
+     if(count2>nums.size()/3){
+        ans.push_back(candidate2);
+    }
+    return ans;
+    }
  }
 
