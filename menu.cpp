2bb34daeab50ed8552566e4ee0e9aa1e689537a1
Abdul21-dev <abdulrahman21062004@gmail.com>
2026-03-04 19:23:26 +0530

    Leetcode 961

--- menu.cpp
+++ menu.cpp
@@ -3004,45 +3004,60 @@ int main(){
        
     //     return arr;
 
-    int count1 = 0;
-    int count2 = 0;
-    int candidate1 = 0;
-    int candidate2 = 0;
-    for(int i=0; i<nums.size(); i++){
-        if(candidate1 == nums[i]){
-            count1++;
-        }else if(candidate2 == nums[i]){
-            count2++;
-        }else if(count1 == 0){
-            candidate1 = nums[i];
-            count1=1;
-        }else if(count2 == 0){
-            candidate2 = nums[i];
-             count2=1;
-        }else{
-            count1--;
-            count2--;
-        }
-    }
+    // int count1 = 0;
+    // int count2 = 0;
+    // int candidate1 = 0;
+    // int candidate2 = 0;
+    // for(int i=0; i<nums.size(); i++){
+    //     if(candidate1 == nums[i]){
+    //         count1++;
+    //     }else if(candidate2 == nums[i]){
+    //         count2++;
+    //     }else if(count1 == 0){
+    //         candidate1 = nums[i];
+    //         count1=1;
+    //     }else if(count2 == 0){
+    //         candidate2 = nums[i];
+    //          count2=1;
+    //     }else{
+    //         count1--;
+    //         count2--;
+    //     }
+    // }
     
-    count1 =0;
-    count2 = 0;
-    for(int i=0; i<nums.size(); i++){
-        if(nums[i]==candidate1){
-            count1++;
-        }else if(nums[i]==candidate2){
-            count2++;
-        }
-    }
+    // count1 =0;
+    // count2 = 0;
+    // for(int i=0; i<nums.size(); i++){
+    //     if(nums[i]==candidate1){
+    //         count1++;
+    //     }else if(nums[i]==candidate2){
+    //         count2++;
+    //     }
+    // }
 
-    vector<int>ans;
-    if(count1>nums.size()/3){
-        ans.push_back(candidate1);
-    }
-     if(count2>nums.size()/3){
-        ans.push_back(candidate2);
-    }
-    return ans;
+    // vector<int>ans;
+    // if(count1>nums.size()/3){
+    //     ans.push_back(candidate1);
+    // }
+    //  if(count2>nums.size()/3){
+    //     ans.push_back(candidate2);
+    // }
+    // return ans;
+    // }
+
+    // Leetcode 961
+     int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int,int>hash;
+        int n = nums.size()/2;
+        for(int i=0; i<nums.size(); i++){
+            hash[nums[i]]++;
+        }
+        for(auto p: hash){
+            if(p.second==n){
+                return p.first;
+            }
+        }
+        return '/0';
     }
  }
 
