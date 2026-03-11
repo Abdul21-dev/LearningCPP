74b6403cf43fb7aa7de8566665df7f17dcfa455b
Abdul21-dev <abdulrahman21062004@gmail.com>
2026-03-11 19:14:46 +0530

    Leetcode 167

--- menu.cpp
+++ menu.cpp
@@ -3212,9 +3212,31 @@ int main(){
     // }
 
     // Leetcode 50
-     double myPow(double x, int n) {
-        return round(pow(x,n)*100000)/100000;
+    //  double myPow(double x, int n) {
+    //     return round(pow(x,n)*100000)/100000;
        
+    // }
+
+    // Leetcode 167
+    vector<int> twoSum(vector<int>& numbers, int target) {
+     //   vector<int>ans;
+       int s = 0;
+       int e = numbers.size()-1;
+       while(s<e){
+        int sum = numbers[s]+numbers[e];
+        if(sum==target){
+            // ans.push_back(s+1);
+            // ans.push_back(e+1);
+           
+             break;
+        }else if(sum<target){
+            s++;
+        }else{
+            e--;
+        }
+       }
+       // return ans;
+      return {s+1, e+1};
     }
  }
 
